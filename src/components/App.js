c928274f6eb76f66a334b82f138d62a6c8cf61d2
Tomas Klatt <tomas.klatt@livesport.eu>
2022-03-23 14:03:35 +0100

    added search bar and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from "axios";
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            headers: {
+                Authorization: `Client-ID ESPzNcgiJQP_xEyAZ6MBvYX64DyjFh4Q3Bof4VPzMFo`
+            },
+            params: {
+                query: term
+            }
+        })
     }
 
     render() {
